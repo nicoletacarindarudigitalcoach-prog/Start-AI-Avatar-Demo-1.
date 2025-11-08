bf9a7e0e9ac8bb5f14e32f15f84eadab4aa09e7c
nicoleta carindaru <nicoletacarindaru@MACBOOKAIR.station>
2025-11-08 09:52:42 +0100

    Deploy clean folder: fix 404, correct vercel config

--- start-ai-avatar-demo/api/env.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (_req, res) => {
-  res.setHeader("Cache-Control", "no-store");
-  return res.status(200).json({
-    DEFAULT_AVATAR_NAME: process.env.DEFAULT_AVATAR_NAME || "Alessandra_ProfessionalLook2_public",
-    HAS_HEYGEN: !!process.env.HEYGEN_API_KEY,
-    HAS_OPENAI: !!process.env.OPENAI_API_KEY
-  });
-};
